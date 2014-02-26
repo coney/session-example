09b2bb74e62b0d13eb834b271067ab74e18b902d
Coney Wu <coney.wu@gmail.com>
2014-02-26 17:09:41 +0800

    add send interval and send count for test script

--- io-pattern/send_request.sh
+++ io-pattern/send_request.sh
@@ -3,6 +3,10 @@ function send_request {
   echo send request $1+$2
   time echo receive reply $1+$2=`curl -s localhost:8000/$1+$2`
 }
-for ((i=1;i<=5;++i)); do
+
+COUNT=${1:-5}
+
+for ((i=1;i<=$COUNT;++i)); do
   send_request $i ${i}0 &
+  sleep 0.01
 done
