7303a87597b273bf4306430e0faa7455da3b11ee
Coney Wu <coney.wu@gmail.com>
2015-09-27 22:52:59 +0800

    add al

--- gdb/disassembly/arguments.cpp
+++ gdb/disassembly/arguments.cpp
@@ -2,46 +2,44 @@
 #include <string.h>
 #include <time.h>
 
-struct Student
-{
-    unsigned long   id;
-    unsigned int    age;
-    unsigned int    score;
-    char            name[16];
-};
+    struct Student
+    {
+        unsigned long   id;
+        unsigned int    age;
+        unsigned int    score;
+        char            name[16];
+    };
 
-struct StudentToken {
-    unsigned long id;
-    unsigned int timeStamp;
-};
+    struct StudentToken {
+        unsigned long id;
+        unsigned int timeStamp;
+    };
 
-class StudentMgmt {
-public:
-    StudentMgmt()
-        : tokenSecret_(0){
+    class StudentMgmt {
+    public:
+        StudentMgmt() : tokenSecret_(0) {}
+        void setTokenSecret(unsigned long secret) {
+            tokenSecret_ = secret;
+        }
+        StudentToken createToken(Student s) {
+            StudentToken token;
+            token.id = tokenSecret_ ^ s.id;
+            token.timeStamp = time(NULL);
+            return token;
+        }
+        unsigned long tokenSecret_;
+    };
+
+    int main(int argc, char **argv) {
+        Student s;
+        s.id = 9527;
+        s.age = 18;
+        s.score = 100;
+        strcpy(s.name, "xiaoming");
+
+        StudentMgmt mgmt;
+        mgmt.setTokenSecret(0xfedcba9876543210UL);
+        StudentToken token = mgmt.createToken(s);
+
+        printf("id:%zx time:%u", token.id, token.timeStamp);
     }
-    void setTokenSecret(unsigned long secret) {
-        tokenSecret_ = secret;
-    }
-    StudentToken createToken(Student s) {
-        StudentToken token;
-        token.id = tokenSecret_ ^ s.id;
-        token.timeStamp = time(NULL);
-        return token;
-    }
-    unsigned long tokenSecret_;
-};
-
-int main(int argc, char **argv) {
-    Student s;
-    s.id = 9527;
-    s.age = 18;
-    s.score = 100;
-    strcpy(s.name, "xiaoming");
-
-    StudentMgmt mgmt;
-    mgmt.setTokenSecret(0xfedcba9876543210UL);
-    StudentToken token = mgmt.createToken(s);
-
-    printf("id:%zx time:%u", token.id, token.timeStamp);
-}
--- gdb/disassembly/build-frame.cpp
+++ gdb/disassembly/build-frame.cpp
@@ -2,17 +2,17 @@
 #include <pthread.h>
 #include <stdio.h>
 
-int sum(int val) {
-    if (val == 1) {
-        return 1;
+    int sum(int val) {
+        if (val == 1) {
+            return 1;
+        }
+        return val + sum(val - 1);
     }
-    return val + sum(val - 1);
-}
 
 
-int main(int argc, char **argv) {
+    int main(int argc, char **argv) {
 
-    printf("sum(4)=%d\n", sum(4));
-	
-    return 0;
-}
+        printf("sum(4)=%d\n", sum(4));
+
+        return 0;
+    }
