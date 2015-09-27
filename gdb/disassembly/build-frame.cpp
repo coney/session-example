3b72f99815196752484cbd1bb59bdebe83dc6fd7
Coney Wu <coney.wu@gmail.com>
2015-09-27 22:40:25 +0800

    add all

--- gdb/disassembly/build-frame.cpp
+++ gdb/disassembly/build-frame.cpp
@@ -2,32 +2,17 @@
 #include <pthread.h>
 #include <stdio.h>
 
-
-#define 	ARRAY_SIZE	10
-
-unsigned int arr[ARRAY_SIZE] = {0};
-
-void *thread_proc(void *arg) {
-	size_t index = (size_t)arg;
-	while (true) {
-		arr[index]++;
-		sleep(1);
-	}
+int sum(int val) {
+    if (val == 1) {
+        return 1;
+    }
+    return val + sum(val - 1);
 }
+
 
 int main(int argc, char **argv) {
 
-	for (size_t i = 0; i < ARRAY_SIZE; i++) {
-		pthread_t thread;
-		pthread_create(&thread, NULL, thread_proc, (void *)i);
-	}
-
-	while (true) {
-		for (size_t i = 0; i < ARRAY_SIZE; i++) {
-			printf("%u ", arr[i]);
-		}
-		puts("\n");
-		sleep(5);
-	}
-	return 0;
+    printf("sum(4)=%d\n", sum(4));
+	
+    return 0;
 }
