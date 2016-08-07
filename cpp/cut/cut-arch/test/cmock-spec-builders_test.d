533867c43f43d52308a6e0e35e8c21609c67c053
Coney Wu <coney.wu@gmail.com>
2016-08-08 00:27:14 +0800

    add cut arch

---
+++ cpp/cut/cut-arch/test/cmock-spec-builders_test.d
@@ -0,0 +1,4 @@
+cmock-spec-builders_test.o: test/cmock-spec-builders_test.cpp \
+ gmock/cmock/cmock.h gmock/gmock/gmock.h gmock/gtest/gtest.h \
+ gmock/cmock/cmock-function-mockers.h gmock/cmock/cmock-spec-builders.h \
+ test/foo_mock.h src/foo.h
---
+++ cpp/cut/cut-arch/test/foo_mock.d
@@ -0,0 +1,4 @@
+foo_mock.o: test/foo_mock.cpp test/foo_mock.h gmock/cmock/cmock.h \
+ gmock/gmock/gmock.h gmock/gtest/gtest.h \
+ gmock/cmock/cmock-function-mockers.h gmock/cmock/cmock-spec-builders.h \
+ src/foo.h
