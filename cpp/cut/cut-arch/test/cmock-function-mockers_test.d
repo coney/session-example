6b1c3e89ab48eff1593e1f24654b1d3bb7f21be6
Coney Wu <coney.wu@gmail.com>
2016-08-24 17:23:27 +0800

    remove generated files

--- cpp/cut/cut-arch/test/cmock-function-mockers_test.d
+++
@@ -1,4 +0,0 @@
-cmock-function-mockers_test.o: test/cmock-function-mockers_test.cpp \
- gmock/cmock/cmock.h gmock/gmock/gmock.h gmock/gtest/gtest.h \
- gmock/cmock/cmock-function-mockers.h gmock/cmock/cmock-spec-builders.h \
- test/foo_mock.h src/foo.h
--- cpp/cut/cut-arch/test/cmock-spec-builders_test.d
+++
@@ -1,4 +0,0 @@
-cmock-spec-builders_test.o: test/cmock-spec-builders_test.cpp \
- gmock/cmock/cmock.h gmock/gmock/gmock.h gmock/gtest/gtest.h \
- gmock/cmock/cmock-function-mockers.h gmock/cmock/cmock-spec-builders.h \
- test/foo_mock.h src/foo.h
